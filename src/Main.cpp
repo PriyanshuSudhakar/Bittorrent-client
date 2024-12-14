41f0e130cd1b26402d6fc5048eacd9018816656a
priyanshu <priyanshugg12@gmail.com>
2024-12-15 01:58:36 +0530

    [any message]

--- src/Main.cpp
+++ src/Main.cpp
@@ -223,7 +223,9 @@ int main(int argc, char* argv[]) {
                         std::string url = decoded_info.at("announce").get<std::string>();
                         // SHA1 sha1;
                         sha1.update(bencoded_string);
-                        std::string encoded_info_hash = url_encode(sha1.final());
+                        std::string encoded_info_hash = sha1.final();
+                        // std::cout<<encoded_info_hash<<std::endl;
+                        std::cout<<encoded_info_hash<<std::endl;
                         // std::string encoded_info_hash = url_encode(sha1(bencoded_string));
                         std::string left = std::to_string(file_data.size()); // Convert size_t to string
                         std::string peer_id = generate_random_peer_id();
